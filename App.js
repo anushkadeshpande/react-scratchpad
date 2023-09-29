6a35298762bdae5841e6019c0b2b070c5ba5479e
anushkadeshpande <anushkaa5000@gmail.com>
2023-09-29 23:54:34 +0530

    added a complex nested structure

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-/* 
-* 1. creating a simple h1 tag
-*/
+/*
+ * 1. creating a simple h1 tag
+ */
 
 const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
 console.log(heading); // logs an object (react element)
-
 
 /* 
 * 2. creating a nested element of format:
@@ -25,9 +24,8 @@ const parent = React.createElement(
   )
 );
 
-
 /* 
-* 2. creating an element with siblings:
+* 3. creating an element with siblings:
     <div id="parent-div">
         <div id="child-div">
             <h1></h1>
@@ -37,21 +35,42 @@ const parent = React.createElement(
 */
 
 const parent2 = React.createElement(
-    "div",
-    { id: "parent-div" },
-    React.createElement(
-      "div",
-      { id: "child-div" },
-      [
-        React.createElement("h1", { id: "nested-h1" }, "I am a nested h1"),
-        React.createElement("h2", { id: "nested-h2" }, "I am a nested h2")
-      ]
-    )
-  );
+  "div",
+  { id: "parent-div" },
+  React.createElement("div", { id: "child-div" }, [
+    React.createElement("h1", { id: "nested-h1" }, "I am a nested h1"),
+    React.createElement("h2", { id: "nested-h2" }, "I am a nested h2"),
+  ])
+);
 
+/* 
+* 4. creating an even more complex structure like:
+    <div id="parent-div">
+        <div id="child-div">
+            <h1></h1>
+            <h2></h2>
+        </div>
+
+        <div id="child-div-2">
+            <h1></h1>
+            <h2></h2>
+        </div>
+    </div>
+*/
+
+const parent3 = React.createElement("div", { id: "parent-div" }, [
+  React.createElement("div", { id: "child-div" }, [
+    React.createElement("h1", { id: "nested-h1" }, "I am a nested h1"),
+    React.createElement("h2", { id: "nested-h2" }, "I am a nested h2"),
+  ]),
+  React.createElement("div", { id: "child-div-2" }, [
+    React.createElement("h1", { id: "nested-h1-2" }, "I am a nested h1-2"),
+    React.createElement("h2", { id: "nested-h2-2" }, "I am a nested h2-2"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-/* 
-* converts the react element (object) ==> html
-*/
-root.render(parent2);
+/*
+ * converts the react element (object) ==> html
+ */
+root.render(parent3);
