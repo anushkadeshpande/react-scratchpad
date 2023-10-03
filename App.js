aaf96ea0f1a0de44516e65cb7f37eb2c92fb464b
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-03 06:00:48 +0530

    feat: added a functional component

--- App.js
+++ App.js
@@ -8,12 +8,23 @@ import ReactDOM from 'react-dom/client';
 const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
 console.log(heading); // logs an object (react element)
 
+// JSX
+// transpiled before reaches to JS Engine (via Parcel - Babel)
+// task of babel : JSX -> React.createElement
+
+// JSX => React.createElement => React element (object) => HTML Element
 const jsxHeading = <h1>Helllloooooo 🚀</h1>
 
 console.log(jsxHeading)
+
+// * CREATING FUNCTIONAL COMPONENT
+
+const HeadingComponent = () => {
+    return <h1>Helllloooooo 🚀</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 /*
  * converts the react element (object) ==> html
  */
-root.render(jsxHeading);
+root.render(HeadingComponent());
