4df2690eea7174bbb4428637b75799dc3bcdb74d
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-04 21:52:39 +0530

    feat: Added App component

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
+const App = () => {
+  return (
+    <></>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<App />);
