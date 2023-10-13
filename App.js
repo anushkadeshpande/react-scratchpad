0903500f9288258fe442d2c88795e83f94598040
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-13 23:14:40 +0530

    docs: added tentative app structure

--- App.js
+++ App.js
@@ -3,9 +3,23 @@ import ReactDOM from "react-dom/client";
 
 const App = () => {
   return (
-    <></>
+    <>
+    {/* Header */}
+      {/* Logo */}
+      {/* Nav Items */}
+    {/* Body */}
+      {/* Search */}
+      {/* Restaurant Container */}
+        {/* Restaurant Card */}
+    {/* Footer */}
+      {/* Copyright */}
+      {/* Links */}
+      {/* Address */}
+      {/* Contact */}
+    </>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+ 
