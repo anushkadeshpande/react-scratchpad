52ea12e411c8502cfc927d8626dc3916606fe866
anushkadeshpande <anushkaa5000@gmail.com>
2024-03-23 15:47:55 +0530

    feat: Used useRouteError to display error infomation on the error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom'
 
 const Error = () => {
+  // useRouteError is a hook provided by react-router-dom to return error information
+
+  const err = useRouteError()
+  console.log(err)
   return (
-    <h1>Oops! Something went wrong!</h1>
+    <>
+      <h1>Oops! Something went wrong!</h1>
+      <h3>{err.status} {err.statusText}</h3>
+      <h3>{err.data}</h3>
+    </>
   )
 }
 
