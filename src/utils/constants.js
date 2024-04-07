051b930d4d65adfc199349a897b57844530ebabf
anushkadeshpande <anushkaa5000@gmail.com>
2024-04-07 16:22:10 +0530

    fix: Updated CDN_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200/"
+// const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200/"
+const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 const LOGO_URL = "https://firebasestorage.googleapis.com/v0/b/github-readme-content.appspot.com/o/fast-food-outline.svg?alt=media&token=8b793cf4-ad2b-485b-aae6-27debbf8b2e6&_gl=1*1q9v9y*_ga*MjAxOTg3ODU3Mi4xNjg1ODU4Nzkz*_ga_CW55HF8NVT*MTY5NzMyMTI2MS4xMS4xLjE2OTczMjEyOTAuMzEuMC4w"
 
 export {CDN_URL, LOGO_URL}
