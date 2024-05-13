c852c638f10900140721baca6a0de3f74e713e8f
anushkadeshpande <anushkaa5000@gmail.com>
2024-05-13 08:17:05 +0530

    feat: Added state variables in class based component

--- food-app/src/components/UserClass.js
+++ food-app/src/components/UserClass.js
@@ -4,12 +4,25 @@ class UserClass extends React.Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      count: 0
+    }
   }
 
   render() {
     return (
       <div>
         <h1>{this.props.name}</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={() => {this.setState({
+          count: this.state.count + 1
+        })}}>Increment</button>
+
+        <button onClick={() => {this.setState({
+          count: this.state.count - 1
+        })}}>Decrement</button>
+
       </div>
     )
   }
